f570e6786d872cc1bf8ab786c5874510f0930af0
Ahmet Fatih Çopur <afatih98@hotmail.com>
2024-01-25 11:39:25 +0300

    Add files via upload

---
+++ remote-app/src/app/counter/counter.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.scss'
+})
+export class CounterComponent {
+
+  public counter = 0;
+
+  constructor(){
+    window.addEventListener("controlMfeCounter", ()=>{
+      this.handleCounter();
+    })
+  }
+
+  handleCounter(){
+    this.counter+=1;
+  }
+}
